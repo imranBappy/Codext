4116ca5ccefcd8304b43f12e9bb15cb1c804d5a4
imranBappy <imranbappy.official@gmail.com>
2023-09-29 00:25:05 +0600

    added slider

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-function main() {
+function navbar() {
   const small_navbar = document.querySelector("#small_navbar");
   const navbar_controller_btn = document.querySelector(
     "#navbar_controller_btn"
@@ -7,6 +7,22 @@ function main() {
   navbar_controller_btn.addEventListener("click", () => {
     small_navbar.classList.toggle("hidden");
   });
+}
+
+function slider() {
+  const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+}
+
+function main() {
+  navbar();
+  slider();
 }
 
 window.onload = main;
